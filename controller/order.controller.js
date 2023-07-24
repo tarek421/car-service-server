36363ee62b1874b9701f55ee58073c991fa82683
tarek421 <tarekmahmud740@gmail.com>
2023-07-24 12:32:13 +0600

    someting change

--- controller/order.controller.js
+++ controller/order.controller.js
@@ -2,6 +2,20 @@ const { v4: uuidv4 } = require('uuid');
 const Order = require('../model/order.modal');
 
 
+const verifyToken = async (req, res, next) => {
+    console.log(req.headers?.authorization)
+    if (req.headers?.authorization?.startsWith("Bearer ")) {
+        const token = req.headers.authorization.split(" ")[1];
+        console.log(token);
+        try {
+            const decodedUser = await admin.auth().verifyIdToken(token);
+            req.decodedEmail = decodedUser.email;
+            console.log(decodedUser.email);
+        } catch { }
+    }
+    next();
+};
+
 exports.findAllOrders = async (req, res) => {
     try {
         const orders = await Order.find();
@@ -10,9 +24,6 @@ exports.findAllOrders = async (req, res) => {
         res.status(200).json(error.message);
     }
 }
-
-
-
 
 exports.postOrder = async (req, res) => {
     console.log(req.body.order)
@@ -34,3 +45,25 @@ exports.postOrder = async (req, res) => {
 }
 
 
+exports.orderStatus = verifyToken, async (req, res) => {
+    try {
+        const token = req.headers.authorization;
+        const requesterEmail = req.decodedEmail;
+
+        if (requester) {
+            const requesterAccount = await Order.findOne({
+                email: requesterEmail,
+            });
+
+            if (requesterAccount.role === 'admin' || requesterAccount.role === 'administer') {
+                const { id, value } = req.body;
+                const filter = { _id: ObjectId(id) };
+                const update = { $set: { status: value } };
+                const result = await Order.updateOne(filter, update);
+                res.json(result);
+            }
+        }
+    } catch (error) {
+        res.status(500).json(error.message)
+    }
+}
--- router/order.router.js
+++ router/order.router.js
@@ -1,11 +1,12 @@
 
 
 const express = require('express');
-const { postOrder, findAllOrders } = require('../controller/order.controller');
+const { postOrder, findAllOrders, orderStatus } = require('../controller/order.controller');
 const orderRouter = express.Router();
 
 orderRouter.get('/', findAllOrders);
 orderRouter.post('/', postOrder);
+orderRouter.put('/status', orderStatus);
 
 
 module.exports = orderRouter; 
